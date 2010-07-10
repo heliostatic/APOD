201b3e3354d003ffcb39cb610999b1ef54d7269c
Ben Cohen <ben@bencohen.net>
2010-07-10 09:18:44 -0700

    added a jpg sizing library and centering

--- apod.rb
+++ apod.rb
@@ -3,15 +3,68 @@ require "mechanize"
 require "sinatra"
 require "nokogiri"
 require 'open-uri'
+require 'net/http'
+
+class JpegDimensions
+  attr_reader :width, :height
+
+  def initialize(image_path)
+    @uri_split = URI.split(image_path)
+    find_jpeg_size
+  end
+
+  def find_jpeg_size
+    begin
+      http = Net::HTTP.new(@uri_split[2], @uri_split[3])
+      state = 0
+      http.get(@uri_split[5]) do |str|  # this yields strings as each packet arrives
+        str.each_byte do |b|
+          state = case state
+          when 0
+            b == 0xFF ? 1 : 0
+          when 1
+            b >= 0xC0 && b <= 0xC3 ? 2 : 0
+          when 2
+            3
+          when 3
+            4
+          when 4
+            5
+          when 5
+            @height = b * 256
+            6
+          when 6
+            @height += b
+            7
+          when 7
+            @width = b * 256
+            8
+          when 8
+            @width += b
+            break
+          end
+        end
+        break if state == 8  # don't need to fetch any more of the image
+      end
+    rescue Exception=>e
+      # I do nothing here, but you can do something more useful with the exception if required
+    end
+  end
+end
 
 BASE_URL = "http://antwrp.gsfc.nasa.gov/apod/"
 
 helpers do
   def get_todays_image
+    @todays_image = {}
     doc = Nokogiri::HTML(open(BASE_URL))
     doc.css('p > a').each do |link|
       link_url = link.attributes["href"]
-      if /image.*\.jpg/ =~ link_url then @todays_image = BASE_URL + link_url end
+      if /image.*\.jpg/ =~ link_url then 
+        @todays_image["url"] = BASE_URL + link_url
+        jpg_info = JpegDimensions.new(@todays_image["url"])
+        @todays_image["width"] = jpg_info.width
+      end
     end
     @todays_image
   end
@@ -43,17 +96,21 @@ __END__
       background:url("http://people.ischool.berkeley.edu/~bcohen/sfemail/background.jpg")
     }
     #content {
-      max-width:90%;
-      margin:auto;
       margin-top: 2em;
     }
     img {
       border: 1px black darkgrey;
       padding: 2px;
-      max-height:600px;
-      -moz-box-shadow: 3px 3px 3px #666;
+      -moz-box-shadow: 0px 0px 10px #666;
       -webkit-box-shadow: 0px 0px 10px #666;
-      box-shadow: 3px 3px 3px #666;
+      box-shadow: 0px 0px 10px #666;;
+    }
+    #apod_image {
+      margin-left:auto;
+      margin-right:auto;
+      display:block;
+      width: <%= @todays_image["width"] %>;
+      text-align:center;
     }
   </style>
 </head>
@@ -65,4 +122,6 @@ __END__
 </html>
 
 @@ index
-<img src="<%= @todays_image %>" />
+<section id="apod_image">
+  <img src="<%= @todays_image["url"] %>" />
+</section>
